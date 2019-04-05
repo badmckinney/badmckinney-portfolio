1536326fc328da72ca94d7e164e103427c42b952
Brad McKinney <brad.mckin@gmail.com>
2019-04-04 22:49:34 -1000

    customize 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,19 @@
 import React from "react"
+import { Link } from "gatsby";
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="Oops!" />
+    <div id='oopss'>
+      <div id='error-text'>
+        <img src="https://cdn.rawgit.com/ahmedhosna95/upload/1731955f/sad404.svg" alt="404" id="img404" />
+        <span id="span404">This page doesn't exist</span>
+        <Link to="/"><div class="back">Back to safety</div></Link>
+      </div>
+    </div>
   </Layout>
 )
 
